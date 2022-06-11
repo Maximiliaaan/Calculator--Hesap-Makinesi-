e4d998c54c652fff7511fd8b856f3a2c75f8f002
Maximiliaaan <plt.arahim.plt@gmail.com>
2022-06-11 13:22:30 +0300

    operations

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const btnContainer = document.querySelector('.buttons-container');
 
 let currOperand = '';
 let previousOperand = '';
+let operation = '';
 
 //? Butonlari tasiyan container icin event tanimlamasi
 btnContainer.addEventListener('click', (e) => {
@@ -15,19 +16,74 @@ btnContainer.addEventListener('click', (e) => {
     appendNumber(e.target.textContent);
     updateDisplay();
   }
+
+  if (e.target.classList.contains('operator')) {
+    chooseOperator(e.target.textContent);
+    updateDisplay();
+  }
 });
 
 const appendNumber = (num) => {
-  //? Eger ilk olarak  0 girilmisse geri don
-  if (!currOperand && num === '0') return;
+  //? Eger onceden 0 girilmisse ve tekrardan 0 girilise geri don
+  if (currOperand === '0' && num === '0') return;
+
+  //? Eğer ilk olarak 0 girilmisse ve sonrasinda da . haricinde baska
+  //? bir sayi girilmis ise sadece girilen yeni sayiyi degiskene aktar.
+  //? Orn: 09 => 9 , 03 => 3 , 0.1 => 0.1
+  if (currOperand === '0' && num !== '.') {
+    currOperand = num;
+    return;
+  }
 
   //? Eğer şu anki sayi . ise ve önceki girilen sayi . iceriyorsa geri don
   if (num === '.' && currOperand.includes('.')) return;
 
+  if (currOperand.length > 10) return;
   //? Girilen sayilari birlestir.
   currOperand += num;
 };
 
 const updateDisplay = () => {
   currDisp.textContent = currOperand;
+  prevDisp.textContent = `${previousOperand} ${operation}`;
+};
+
+const chooseOperator = (op) => {
+  //? ilk sayi girisiinden sonraki islemleri gercekletir
+  if (previousOperand) {
+    calculate();
+  }
+
+  //? Degisken swapiing
+  operation = op;
+  previousOperand = currOperand;
+  currOperand = '';
+};
+
+const calculate = () => {
+  let calculation = 0;
+
+  const prev = Number(previousOperand);
+  const current = Number(currOperand);
+
+  console.log(prev, current);
+
+  switch (operation) {
+    case '+':
+      calculation = prev + current;
+      break;
+    case '-':
+      calculation = prev - current;
+      break;
+    case 'x':
+      calculation = prev * current;
+      break;
+    case '÷':
+      calculation = prev / current;
+      break;
+    default:
+      break;
+  }
+
+  currOperand = calculation;
 };
